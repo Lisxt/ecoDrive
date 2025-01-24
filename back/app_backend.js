fd3614eed52224315eb6bc4fee6ba39604b5949d
gustavo077729 <gustavo.xtavares@a.ucb.br>
2025-01-24 08:55:08 -0300

    alteração

--- back/app_backend.js
+++ back/app_backend.js
@@ -16,7 +16,7 @@ app.use(cors());
 const bancoDeDados = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'c@tolic@',
     database: 'ecoDrive'
 })
 
